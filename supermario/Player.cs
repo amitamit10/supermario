1de410f4cfb4dd07fd91d2237c636170f8a208fd
amit elgabsi <amit.elgabsy@gmail.com>
2025-12-01 13:26:48 +0200

    improved from almost scratch#

--- supermario/Player.cs
+++ supermario/Player.cs
@@ -3,61 +3,58 @@ using System.Windows.Forms;
 
 namespace supermario
 {
-    public class Player 
+public class Player
+{
+public int Health { get; set; }
+public int Score { get; set; }
+public Point Position { get; set; }
+public bool IsGrounded { get; set; }
+
+private float VerticalVelocity;
+private const int MoveSpeed = 5;
+private const float Gravity = 1.0f;
+private const int JumpStrength = 15;
+private const int GroundLevelY = 400;
+private Point StartPosition;
+
+public Player(Point startPosition, Image playerImage)
+{
+    Health = 1;
+    Score = 0;
+    IsGrounded = true;
+    VerticalVelocity = 0;
+    Position = startPosition;
+    StartPosition = startPosition;
+}
+
+public void Move(int direction, bool shouldJump)
+{
+    int newX = Position.X + (direction * MoveSpeed);
+    Position = new Point(newX, Position.Y);
+
+    if (shouldJump && IsGrounded)
     {
-        public int Health { get; private set; }
-        public int Score { get; private set; }
-        public bool IsGrounded { get; private set; }
-        public Point Position { get; private set; }
-
-        private float VerticalVelocity;
-        private const int MoveSpeed = 5;
-        private const float Gravity = 1.0f;
-        private const int GroundLevelY = 400;
-
-        public Player(Point startPosition, Image playerImage)
-           
-        {
-            Health = 1;
-            Score = 0;
-            IsGrounded = true;
-            VerticalVelocity = 0;
-        }
-
-        public void Move(int direction)
-        {
-            int newX = Position.X + (direction * MoveSpeed);
-
-            Position = new Point(newX, Position.Y);
-        }
-
-        public void Jump()
-        {
-            if (IsGrounded)
-            {
-                IsGrounded = false;
-                VerticalVelocity = -1;
-            }
-        }
-
-        public  void Update(float deltaTime)
-        {
-            
-        }
-
-        public void CollectCoin()
-        {
-            Score += 10;
-        }
-
-        public  void TakeDamage(int amount)
-        {
-            Health -= amount;
-        }
-
-        public void Respawn(Point startPosition)
-        {
-           
-        }
+        IsGrounded = false;
+        VerticalVelocity = -JumpStrength;
     }
+}
+
+public void SetCoin(int coinValue)
+{
+    Score += coinValue;
+}
+
+public void SetDamage(int damageAmount)
+{
+    Health -= damageAmount;
+}
+
+public void Respawn()
+{
+    Position = StartPosition;
+    Health = 1;
+    IsGrounded = true;
+    VerticalVelocity = 0;
+}
+}
 }
