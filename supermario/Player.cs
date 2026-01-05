21e783c155d050e0be4721fd53ec2ff455fa2f85
amitamit10 <147654153+amitamit10@users.noreply.github.com>
2026-01-05 12:00:52 +0200

    fixed some stuff

--- supermario/Player.cs
+++ supermario/Player.cs
@@ -3,58 +3,170 @@ using System.Windows.Forms;
 
 namespace supermario
 {
-public class Player
-{
-public int Health { get; set; }
-public int Score { get; set; }
-public Point Position { get; set; }
-public bool IsGrounded { get; set; }
+    public class Player
+    {
+        public int Health { get; set; }
+        public int Score { get; set; }
+        public bool IsGrounded { get; set; }
+        public Point Position { get; set; }
 
         private float VerticalVelocity;
-private const int MoveSpeed = 5;
-private const float Gravity = 1.0f;
-private const int JumpStrength = 15;
-private const int GroundLevelY = 400;
-private Point StartPosition;
+        private float HorizontalVelocity;
 
-public Player(Point startPosition, Image playerImage)
-{
-    Health = 1;
-    Score = 0;
-    IsGrounded = true;
-    VerticalVelocity = 0;
-    Position = startPosition;
-    StartPosition = startPosition;
-}
+        // Improved physics constants
+        private const float MoveSpeed = 6f;
+        private const float MaxMoveSpeed = 8f;
+        private const float Acceleration = 1.2f;
+        private const float Deceleration = 0.8f;
+        private const float AirControl = 0.6f; // Control while in air
 
-public void Move(int direction, bool shouldJump)
-{
-    int newX = Position.X + (direction * MoveSpeed);
-    Position = new Point(newX, Position.Y);
+        private const float Gravity = 0.6f;
+        private const float JumpPower = -13f;
+        private const float MaxFallSpeed = 15f;
 
-    if (shouldJump && IsGrounded)
-    {
-        IsGrounded = false;
-        VerticalVelocity = -JumpStrength;
+        // Variable jump height (hold jump longer = jump higher)
+        private const float JumpReleaseGravityMultiplier = 2f;
+        private bool isJumpHeld = false;
+
+        public Player(Point startPosition, Image playerImage)
+        {
+            Health = 3;
+            Score = 0;
+            IsGrounded = false;
+            Position = startPosition;
+            VerticalVelocity = 0;
+            HorizontalVelocity = 0;
+        }
+
+        public void Move(int direction, bool shouldJump)
+        {
+            // Horizontal movement with acceleration/deceleration
+            float targetSpeed = direction * MoveSpeed;
+            float accelRate = (IsGrounded ? Acceleration : Acceleration * AirControl);
+
+            if (direction != 0)
+            {
+                // Accelerate towards target speed
+                if (HorizontalVelocity < targetSpeed)
+                {
+                    HorizontalVelocity += accelRate;
+                    if (HorizontalVelocity > targetSpeed) HorizontalVelocity = targetSpeed;
+                }
+                else if (HorizontalVelocity > targetSpeed)
+                {
+                    HorizontalVelocity -= accelRate;
+                    if (HorizontalVelocity < targetSpeed) HorizontalVelocity = targetSpeed;
+                }
+            }
+            else
+            {
+                // Decelerate when no input
+                if (HorizontalVelocity > 0)
+                {
+                    HorizontalVelocity -= Deceleration;
+                    if (HorizontalVelocity < 0) HorizontalVelocity = 0;
+                }
+                else if (HorizontalVelocity < 0)
+                {
+                    HorizontalVelocity += Deceleration;
+                    if (HorizontalVelocity > 0) HorizontalVelocity = 0;
+                }
+            }
+
+            // Cap horizontal speed
+            if (HorizontalVelocity > MaxMoveSpeed) HorizontalVelocity = MaxMoveSpeed;
+            if (HorizontalVelocity < -MaxMoveSpeed) HorizontalVelocity = -MaxMoveSpeed;
+
+            // Apply horizontal movement
+            int newX = Position.X + (int)HorizontalVelocity;
+            if (newX >= 0 && newX <= 2950)
+            {
+                Position = new Point(newX, Position.Y);
+            }
+
+            // Jump logic with variable height
+            if (shouldJump && IsGrounded)
+            {
+                Jump();
+                isJumpHeld = true;
+            }
+            else if (!shouldJump && VerticalVelocity < 0)
+            {
+                // Released jump early - apply extra gravity for shorter jump
+                isJumpHeld = false;
+            }
+
+            // Apply gravity
+            if (!IsGrounded)
+            {
+                float gravityToApply = Gravity;
+
+                // Apply extra gravity if jump released early (for variable jump height)
+                if (!isJumpHeld && VerticalVelocity < 0)
+                {
+                    gravityToApply *= JumpReleaseGravityMultiplier;
+                }
+
+                VerticalVelocity += gravityToApply;
+
+                // Cap fall speed
+                if (VerticalVelocity > MaxFallSpeed)
+                {
+                    VerticalVelocity = MaxFallSpeed;
+                }
+
+                // Apply vertical velocity
+                int newY = Position.Y + (int)VerticalVelocity;
+                Position = new Point(Position.X, newY);
+            }
+            else
+            {
+                VerticalVelocity = 0;
+            }
+
+            // Floor collision (safety net)
+            if (Position.Y >= 450)
+            {
+                Position = new Point(Position.X, 450);
+                IsGrounded = true;
+                VerticalVelocity = 0;
+            }
+        }
+
+        public void Jump()
+        {
+            if (IsGrounded)
+            {
+                IsGrounded = false;
+                VerticalVelocity = JumpPower;
+                isJumpHeld = true;
+            }
+        }
+
+        public void Update(float deltaTime)
+        {
+            // Reserved for future use
+        }
+
+        public void CollectCoin()
+        {
+            Score += 10;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+            if (Health < 0) Health = 0;
+        }
+
+        public void Respawn(Point startPosition)
+        {
+            Position = startPosition;
+            Health = 3;
+            VerticalVelocity = 0;
+            HorizontalVelocity = 0;
+            IsGrounded = false;
+            isJumpHeld = false;
+        }
     }
-}
-
-public void SetCoin(int coinValue)
-{
-    Score += coinValue;
-}
-
-public void SetDamage(int damageAmount)
-{
-    Health -= damageAmount;
-}
-
-public void Respawn()
-{
-    Position = StartPosition;
-    Health = 1;
-    IsGrounded = true;
-    VerticalVelocity = 0;
-}
-}
 }
--- supermario/mainWin.Designer.cs
+++ supermario/mainWin.Designer.cs
@@ -28,20 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.BOTTOM = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.BOTTOM)).BeginInit();
+            this.picboxplayer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxplayer)).BeginInit();
             this.SuspendLayout();
             // 
-            // BOTTOM
+            // picboxplayer
             // 
-            this.BOTTOM.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.BOTTOM.Image = global::supermario.Properties.Resources.oie_cnoULepStRjX;
-            this.BOTTOM.Location = new System.Drawing.Point(-25, 518);
-            this.BOTTOM.Name = "BOTTOM";
-            this.BOTTOM.Size = new System.Drawing.Size(1005, 58);
-            this.BOTTOM.TabIndex = 0;
-            this.BOTTOM.TabStop = false;
+            this.picboxplayer.BackColor = System.Drawing.Color.Transparent;
+            this.picboxplayer.Image = global::supermario.Properties.Resources.dcaeqy1_614416a8_3ae1_4448_94b4_e3ecefa3e53a;
+            this.picboxplayer.Location = new System.Drawing.Point(5, 342);
+            this.picboxplayer.Name = "picboxplayer";
+            this.picboxplayer.Size = new System.Drawing.Size(135, 108);
+            this.picboxplayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxplayer.TabIndex = 1;
+            this.picboxplayer.TabStop = false;
             // 
             // mainWin
             // 
@@ -50,20 +50,18 @@
             this.BackgroundImage = global::supermario.Properties.Resources.oie_RF5qvlenQ7kF1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(982, 553);
-            this.Controls.Add(this.BOTTOM);
+            this.Controls.Add(this.picboxplayer);
             this.MaximumSize = new System.Drawing.Size(1000, 600);
             this.MinimumSize = new System.Drawing.Size(1000, 600);
             this.Name = "mainWin";
-            this.Text = "Form1";
+            this.Text = "Super Mario";
             this.Load += new System.EventHandler(this.mainWin_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.BOTTOM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxplayer)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox BOTTOM;
+        private System.Windows.Forms.PictureBox picboxplayer;
     }
 }
-
