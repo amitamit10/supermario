aded1a26714a877163cf5709e61042068a466239
amitamit10 <147654153+amitamit10@users.noreply.github.com>
2026-01-05 12:05:39 +0200

    update ident

--- supermario/gameObjectS.cs
+++ supermario/gameObjectS.cs
@@ -3,33 +3,33 @@ using System.Drawing;
 
 namespace supermario
 {
-class GameObjectS
-{
-public PictureBox PictureBox { get; private set; }
-public Point Position { get; private set; }
-public string Type { get; private set; }
+    class GameObjectS
+    {
+        public PictureBox PictureBox { get; private set; }
+        public Point Position { get; private set; }
+        public string Type { get; private set; }
 
-public Rectangle Bounds => PictureBox.Bounds;
+        public Rectangle Bounds => PictureBox.Bounds;
 
-public GameObjectS(PictureBox pictureBox, Point position, string type)
-{
-    PictureBox = pictureBox;
-    Position = position;
-    Type = type;
+        public GameObjectS(PictureBox pictureBox, Point position, string type)
+        {
+            PictureBox = pictureBox;
+            Position = position;
+            Type = type;
 
-    PictureBox.Location = position;
-}
+            PictureBox.Location = position;
+        }
 
-public void UpdatePosition(Point newPosition)
-{
-    Position = newPosition;
-    PictureBox.Location = newPosition;
-}
+        public void UpdatePosition(Point newPosition)
+        {
+            Position = newPosition;
+            PictureBox.Location = newPosition;
+        }
 
-public void Move(int deltaX, int deltaY)
-{
-    Point newPosition = new Point(Position.X + deltaX, Position.Y + deltaY);
-    UpdatePosition(newPosition);
-}
-}
+        public void Move(int deltaX, int deltaY)
+        {
+            Point newPosition = new Point(Position.X + deltaX, Position.Y + deltaY);
+            UpdatePosition(newPosition);
+        }
+    }
 }
